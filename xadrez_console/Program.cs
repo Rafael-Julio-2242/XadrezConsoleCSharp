9fb23ad4b36488c0d8c38bae0acb668746f859f6
Rafael Julio <rafaeljuliodsilveira212@gmail.com>
2023-10-16 10:25:51 -0300

    Criando a classe 'ChessPosition'

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -10,20 +10,15 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Board b = new Board(8, 8);
 
-                b.SetBoardPiece(new Tower(b, Color.Black), new Position(0, 0));
-                b.SetBoardPiece(new Tower(b, Color.Black), new Position(1, 9));
-                b.SetBoardPiece(new King(b, Color.Black), new Position(2, 4));
+            ChessPosition pos = new ChessPosition('c',7);
 
-                Screen.PrintBoard(b);
-            }
-           catch (BoardException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+
+
+            Console.WriteLine(pos);
+
+            Console.WriteLine(pos.ToPosition());
+
 
         }
     }
