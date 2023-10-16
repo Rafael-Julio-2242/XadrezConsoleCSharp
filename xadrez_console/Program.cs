1a65fd9d1ce2362d3a40a8c7b976933fdaf19735
Rafael Julio <rafaeljuliodsilveira212@gmail.com>
2023-10-16 09:28:41 -0300

    Criando a classe 'Position'

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+using tabuleiro;
 
 namespace xadrez_console
 {
@@ -6,7 +7,12 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-          
+
+            Position p;
+            p = new Position(3,4);
+
+            Console.WriteLine("Position: " + p);
+
         }
     }
 }
---
+++ xadrez_console/tabuleiro/Position.cs
@@ -0,0 +1,22 @@
+﻿namespace tabuleiro
+{
+    class Position
+    {
+
+        public int Line { get; set; }
+        public int Column { get; set; }
+
+
+        public Position(int line, int Column)
+        {
+            this.Line = line;
+            this.Column = Column;
+        }
+
+        public override string ToString()
+        {
+            return Line + ", " + Column;
+        }
+
+    }
+}
